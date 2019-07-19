451f9fb6eb0bd20cd8a8aa94777eb553edcf250e
ebcitron <45603390+ebcitron@users.noreply.github.com>
2019-07-19 11:23:13 -0500

    Update App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import Cardlist from './components/Cardlist';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [state, setState] = useState();
+  
+  const url = 'https://swapi.co/api/people/';
+
+  useEffect(() => {
+    axios
+    .get(url)
+    .then(res => setState(res.data.results))
+    .catch(err => console.log("Catch Error Retrieving API results: ", err));
+
+  })
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +25,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Cardlist characters = {state} />
     </div>
   );
 }
